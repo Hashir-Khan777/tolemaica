3c0e454c4ba24aa8f37e895a690986e95bec7539
Abdul Waheed <themindwar@gmail.com>
2025-03-19 15:51:49 +0500

    changes by waheed

--- src/components/QrCertSection.jsx
+++ src/components/QrCertSection.jsx
@@ -158,11 +158,15 @@ function QrCertSection() {
                 <div className="flex flex-col gap-[20px]">
                     <Heading1 headingGray="Our" headingWhite="Partners" />
                     <Text>
-                        Lorem ipsum dolor sit amet consectetur.{" "}
-                        <GradientSpan>Lorem mollis est</GradientSpan> nisl enim sed nunc
-                        senectus. Molestie arcu arcu sapien vulputate ligula. Odio dolor
-                        velit vestibulum adipiscing.
-                    </Text>
+                                At Tolemaica, we collaborate with leading organizations across
+                                industries to provide secure, instant, and legally certified digital
+                                solutions. Our trusted partners rely on ourLorem ipsum dolor sit
+                                amet consectetur.{" "}
+                                <GradientSpan>
+                                  IAC (Instant Automatic Certification) technology
+                                </GradientSpan>{" "}
+                                IAC (Instant Automatic Certification) technology
+                              </Text>
                 </div>
 
                 <OurPartnersSlider image1="/img1.svg" image2="/img2.svg" image3="/img3.svg" image4="/img4.svg" />
