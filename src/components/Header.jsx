dd6a5c0bf2df7d6ba0a1158cc32e6dd8f301f0c8
Hashir-Khan777 <khnhashir555@gmail.com>
2025-03-20 20:04:21 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,8 +17,7 @@ function Header() {
     {
       title: "services",
       func: () => setModalIsOpen(true),
-      hasDropdown: true
-
+      hasDropdown: true,
     },
     {
       title: "case  study",
@@ -42,21 +41,21 @@ function Header() {
   // };
   const scrollToFooter = () => {
     // If on a different page, first navigate to the page with the footer
-    if (!window.location.pathname.includes('/')) {
-      navigate('/'); // Navigate to home or page with footer
-      
+    if (!window.location.pathname.includes("/")) {
+      navigate("/"); // Navigate to home or page with footer
+
       // Wait for navigation to complete before scrolling
       setTimeout(() => {
-        const footer = document.getElementById('footer');
+        const footer = document.getElementById("footer");
         if (footer) {
-          footer.scrollIntoView({ behavior: 'smooth' });
+          footer.scrollIntoView({ behavior: "smooth" });
         }
       }, 500);
     } else {
       // If already on the right page, just scroll
-      const footer = document.getElementById('footer');
+      const footer = document.getElementById("footer");
       if (footer) {
-        footer.scrollIntoView({ behavior: 'smooth' });
+        footer.scrollIntoView({ behavior: "smooth" });
       }
     }
   };
@@ -95,10 +94,11 @@ function Header() {
                   to={navLink.link}
                   className={({ isActive }) =>
                     `relative uppercase font-raleway flex items-center gap-4
-    ${isActive
-                      ? "text-white font-[600] xl:text-[20px]/[100%] text-[17px]/[20.48px]"
-                      : "text-white/60 font-[500] xl:text-[16px]/[100%] tracking-[10%] text-[14px]/[16.78px]"
-                    }`
+    ${
+      isActive
+        ? "text-white font-[600] xl:text-[20px]/[100%] text-[17px]/[20.48px]"
+        : "text-white/60 font-[500] xl:text-[16px]/[100%] tracking-[10%] text-[14px]/[16.78px]"
+    }`
                   }
                 >
                   {navLink.link === pathname ? (
@@ -110,19 +110,23 @@ function Header() {
                 <button
                   onClick={navLink.func}
                   index={index}
-                  className={`flex items-center gap-4 relative cursor-pointer uppercase font-raleway ${pathname.includes(navLink.title)
-                    ? "text-white font-[600] xl:text-[20px]/[100%] text-[17px]/[20.48px]"
-                    : "text-white/60 font-[500] xl:text-[16px]/[100%] tracking-[10%] text-[14px]/[16.78px]"
-                    }`}
+                  className={`flex items-center gap-4 relative cursor-pointer uppercase font-raleway ${
+                    pathname.includes(navLink.title)
+                      ? "text-white font-[600] xl:text-[20px]/[100%] text-[17px]/[20.48px]"
+                      : "text-white/60 font-[500] xl:text-[16px]/[100%] tracking-[10%] text-[14px]/[16.78px]"
+                  }`}
                 >
                   {pathname.includes(navLink.title) ? (
                     <div className="bg-[#FF9966] w-[10px] h-[10px] rounded-full"></div>
                   ) : null}
                   {navLink.title}
                   {navLink.hasDropdown && (
-                    <img src="/dropdown.svg" alt="Dropdown" className="w-4 h-4 ml-1" />
+                    <img
+                      src="/dropdown.svg"
+                      alt="Dropdown"
+                      className="w-4 h-4 ml-1"
+                    />
                   )}
-
                 </button>
               )}
               {/* <span className="text-gray-400">/</span> */}
@@ -130,14 +134,19 @@ function Header() {
             </div>
           ))}
 
-          <a href="https://ddeluca4.wixsite.com/solid-srl" trarget="_blank" className="text-yellow-400 shrink-0 text-sm">
+          <a
+            href="https://ddeluca4.wixsite.com/solid-srl"
+            target="_blank"
+            className="text-yellow-400 shrink-0 text-sm"
+          >
             <img src="/solid.png" alt="" className="h-[20px] w-auto" />
           </a>
           <span className="text-white/60">/</span>
           {/* Contact Button */}
           <button
             onClick={scrollToFooter}
-            className="cursor-pointer text-black text-[16px]/[18.78px] bg-white rounded-full font-[700] py-[10px] px-[24px]">
+            className="cursor-pointer text-black text-[16px]/[18.78px] bg-white rounded-full font-[700] py-[10px] px-[24px]"
+          >
             CONTACT
           </button>
         </nav>
@@ -157,8 +166,9 @@ function Header() {
 
       {/* Mobile Sidebar */}
       <div
-        className={`fixed z-50 top-0 right-0 h-full w-full backdrop-blur-3xl bCK flex flex-col justify-center items-center bg-black text-white p-6 transform ${isOpen ? "translate-x-0" : "translate-x-full"
-          } transition-transform duration-300 ease-in-out lg:hidden rounded-l-lg`}
+        className={`fixed z-50 top-0 right-0 h-full w-full backdrop-blur-3xl bCK flex flex-col justify-center items-center bg-black text-white p-6 transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out lg:hidden rounded-l-lg`}
         style={{
           background:
             " linear-gradient(to left bottom, #8f644f, #48423f 80%, #4c2a34 )",
@@ -209,12 +219,13 @@ function Header() {
             about
           </Link>
 
-          <Link
+          <a
+            href="https://ddeluca4.wixsite.com/solid-srl"
+            target="_blank"
             className="h-[52px] w-[300px] rounded-full border-[2.1px] text-white border-white flex justify-center items-center font-raleway font-medium md:text-[20px]/[100%] py-[12px] px-[40px]"
-            to="/"
           >
             <img src="/solid.png" alt="Solid" className="h-6 w-auto" />
-          </Link>
+          </a>
         </nav>
       </div>
       <ServiceModal
